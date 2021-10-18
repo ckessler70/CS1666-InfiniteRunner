6cfa623b53660b7b7d75919375836619913dee49
DaneHalle <29028364+DaneHalle@users.noreply.github.com>
2021-10-18 14:10:43 -0400

    Backflipping Lizard

--- inf_runner/src/demo.rs
+++ inf_runner/src/demo.rs
@@ -311,12 +311,12 @@ impl Game for Demo {
 
             //ADDITION: Hey lizard, do a flip
             r_flip_spot = if r_flip{
-                r_flip_spot + 30.0  //decrement for faster flip speed
+                r_flip_spot - 30.0  //decrement for faster flip speed
             } else{
                 0.0
             };
 
-            if r_flip_spot == 360.0{ //flip complete
+            if r_flip_spot == -360.0{ //flip complete
                 r_flip = false;
                 r_flip_spot == 0.0; //reset flip_spot
             }
