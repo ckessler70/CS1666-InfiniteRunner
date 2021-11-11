f4e278a416afb62d99ead0e054d935d068feb58f
Wiezel77 <56902128+Wiezel77@users.noreply.github.com>
2021-11-11 09:56:36 -0500

    Backend bouncy terrain progress

--- src/runner.rs
+++ src/runner.rs
@@ -128,7 +128,7 @@ impl Game for Runner {
         // bg[2] = Ground
         let mut bg: [[i16; SIZE]; 3] = [[0; SIZE]; 3];
 
-        let mut ground_buffer: [(f64, f64); BUFF_LENGTH] = [(0.0, 0.0); BUFF_LENGTH];
+        let mut ground_buffer: [(f64, f64); BUFF_LENGTH + 1] = [(0.0, 0.0); BUFF_LENGTH + 1];
         let mut buff_idx = 0;
 
         let mut rng = rand::thread_rng();
@@ -396,7 +396,7 @@ impl Game for Runner {
 
                 //kinematics change, scroll speed does not :(
                 //can see best when super curvy map generated
-                println!("px:{}  vx:{} ax:{}",player.x(),player.vel_x(),player.accel_x());
+                //println!("px:{}  vx:{} ax:{}",player.x(),player.vel_x(),player.accel_x());
                 //println!("py:{}  vy:{} ay:{}",player.y(),player.vel_y(),player.accel_y());
                 //println!("{}", angle);
 
@@ -433,6 +433,12 @@ impl Game for Runner {
                     bg[GROUND_INDEX][(SIZE - 1) as usize] = ground_buffer[buff_idx].1 as i16;
 
                     buff_idx += 1;
+
+                    if(ground_buffer[ground_buffer.len() - 1] == (1.0, 1.0)){
+                        println!("Bouncy!");
+                    } else {
+                        println!("Not Bouncy!");
+                    }
                 }
 
                 // Every 3 ticks, build a new front mountain segment
